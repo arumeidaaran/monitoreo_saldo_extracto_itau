974a2b5730c4bd4ea83c2ba65dc95b655fcdc96d
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2026-04-09 16:53:11 -0300

    Creado la prueba unitária de función test_entrar_sitio_itau

--- monitoreo_saldo_extracto_itau/tests/src/test_monitoreo_saldo_extracto_itau.py
+++ monitoreo_saldo_extracto_itau/tests/src/test_monitoreo_saldo_extracto_itau.py
@@ -1,11 +1,13 @@
 from monitoreo_saldo_extracto_itau import entrar_sitio_itau
+from py_rpautom.python_utils import processo_existente
 
 def test_entrar_sitio_itau():
     resultado_test_entrar_sitio_itau = entrar_sitio_itau()
 
     assert resultado_test_entrar_sitio_itau == {
         'status': 'done',
-        'reason': f'Función procesada',
-        'data': None
+        'reason': 'Función procesada',
+        'data': True
     }
+    assert processo_existente('chrome') == True
 
