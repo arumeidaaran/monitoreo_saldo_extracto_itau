2c7065f36b951d18264cfc3fca5d4465a75e1136
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2026-05-12 13:46:22 -0300

    Añadida la completitud del flujo de la automatización del sitio Itaú

--- monitoreo_saldo_extracto_itau/src/monitoreo_saldo_extracto_itau.py
+++ monitoreo_saldo_extracto_itau/src/monitoreo_saldo_extracto_itau.py
@@ -3,9 +3,20 @@ from py_rpautom.python_utils import cls
 
 from os import getenv
 
-from utils.pom import entrar_sitio_itau, resolver_login
+from utils.pom import (
+    acceder_elemento_menu_saldo_e_extrato,
+    colectar_campo_valor_extrato,
+    colectar_extrato,
+    entrar_sitio_itau,
+    esperar_loading,
+    resolver_contraseña,
+    resolver_login,
+    resolver_token,
+    validar_campo_valor_extrato_existe,
+    habilitar_campo_valor_extrato,
+)
 
-def executar_fluxo():
+def ejecutar_flujo():
     resultado = {
         'status': '',
         'reason': '',
@@ -13,6 +24,8 @@ def executar_fluxo():
     }
 
     try:
+        valor_extrato = '0,00'
+
         opcion_login = 'AGENCY-ACCOUNT'
         credenciales = {
             'agencia': getenv('MONITOREO_SALDO_EXTRACTO_ITAU_AGENCIA_ITAU'),
@@ -21,6 +34,30 @@ def executar_fluxo():
         contraseñaTeclado = getenv(
             'MONITOREO_SALDO_EXTRACTO_ITAU_CONTRASENA_ITAU'
         )
+
+        if not credenciales['agencia']:
+            resultado['reason'] = (
+                'Agencia no definida correctamente en '
+                'los entornos del sistema'
+            )
+
+            raise SystemError(resultado['reason'])
+
+        if not credenciales['cuenta']:
+            resultado['reason'] = (
+                'Cuenta no definida correctamente en '
+                'los entornos del sistema'
+            )
+
+            raise SystemError(resultado['reason'])
+
+        if not contraseñaTeclado:
+            resultado['reason'] = (
+                'Contraseña del teclado virtual no definida correctamente '
+                'en los entornos del sistema'
+            )
+
+            raise SystemError(resultado['reason'])
 
         entrar_sitio_itau(False)
 
@@ -31,12 +68,80 @@ def executar_fluxo():
         if resultado_resolver_login['status'] == 'undone':
             raise RuntimeError(resultado_resolver_login['reason'])
 
+        resultado_resolver_token = resolver_token()
+        if resultado_resolver_token['status'] == 'undone':
+            raise RuntimeError(resultado_resolver_token['reason'])
+
+        resultado_resolver_contraseña = resolver_contraseña(contraseñaTeclado)
+        if resultado_resolver_contraseña['status'] == 'undone':
+            raise RuntimeError(resultado_resolver_contraseña['reason'])
+
+        validacao_campo_valor_extrato_existe = (
+            validar_campo_valor_extrato_existe()
+        )
+        if validacao_campo_valor_extrato_existe['status'] == 'undone':
+            raise RuntimeError(
+                validacao_campo_valor_extrato_existe['reason']
+            )
+
+        if validacao_campo_valor_extrato_existe['data'] is False:
+            resultado_habilitar_campo_valor_extrato = (
+                habilitar_campo_valor_extrato()
+            )
+            if resultado_habilitar_campo_valor_extrato['status'] == 'undone':
+                raise RuntimeError(
+                    resultado_habilitar_campo_valor_extrato['reason']
+                )
+
+        resultado_colectar_campo_valor_extrato = (
+            colectar_campo_valor_extrato()
+        )
+        if resultado_colectar_campo_valor_extrato['status'] == 'undone':
+            raise RuntimeError(
+                resultado_colectar_campo_valor_extrato['reason']
+            )
+
+        valor_extrato = resultado_colectar_campo_valor_extrato['data']
+
+        while True:
+            try:
+                resultado_acceder_elemento_menu_saldo_e_extrato = (
+                    acceder_elemento_menu_saldo_e_extrato()
+                )
+                if (
+                    resultado_acceder_elemento_menu_saldo_e_extrato[
+                        'status'
+                    ] == 'undone'
+                ):
+                    raise RuntimeError(
+                        resultado_acceder_elemento_menu_saldo_e_extrato[
+                            'reason'
+                        ]
+                    )
+
+                esperar_loading(salir = True, tiempoLimite = 30)
+
+                resultado_colectar_extrato = colectar_extrato()
+                if resultado_colectar_extrato['status'] == 'undone':
+                    raise RuntimeError(
+                        resultado_colectar_extrato['reason']
+                    )
+
+                if not (
+                    valor_extrato ==
+                    resultado_colectar_extrato['data']
+                ):
+                    valor_extrato = resultado_colectar_extrato['data']
+
+                    print('Enviar e-mail')
+            except KeyboardInterrupt:
+                print("Interrumpido por el usuario")
+                break
+            except Exception as error:
+                raise error
+
         resultado['status'] = 'done'
         resultado['reason'] = 'Función procesada'
-        resultado['data'] = {
-            'credenciales': credenciales,
-            'contraseñaTeclado': contraseñaTeclado,
-        }
     except Exception as error:
         resultado['status'] = 'undone'
         resultado['reason'] = str(error)
@@ -45,7 +150,7 @@ def executar_fluxo():
 
 
 def main():
-    resultado = executar_fluxo()
+    resultado = ejecutar_flujo()
     cls()
     print(resultado)
 
